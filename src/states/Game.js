674da2119ad2487405f9285a871819ba9559584d
aegodbold <aegodbold@hotmail.com>
2021-03-10 23:45:53 -0600

    Update Game.js

--- src/states/Game.js
+++ src/states/Game.js
@@ -4,8 +4,10 @@
 import Phaser from 'phaser'
 
 export default class extends Phaser.State {
+	// Very first function called in the state.
 	init () {}
 
+	// Called after init(). Use for loading assets. Do not create objects in this function.
 	preload () {
 		//fixes the sound being suspended without a click in Chrome
 		if (game.sound.context.state === 'suspended') {
@@ -14,5 +16,9 @@ export default class extends Phaser.State {
 		game.stage.disableVisibilityChange = true;  // keeps everything running when the stage is no longer focused
 	}
 
+	// Called immediately after preload() is completed. 
 	create () {}
+	
+	// Updates and redraws your game. Called every frame.
+	update () {}
 }

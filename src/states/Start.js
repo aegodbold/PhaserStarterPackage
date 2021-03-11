6ecfb2b2e660c099769ae625c8fd1b412c05e57a
aegodbold <aegodbold@hotmail.com>
2021-03-10 23:46:16 -0600

    Update Start.js

--- src/states/Start.js
+++ src/states/Start.js
@@ -2,12 +2,24 @@
 //Start Screen
 import Phaser from 'phaser'
 
+import Phaser from 'phaser'
+
 export default class extends Phaser.State {
-  	 init () {}
+	// Very first function called in the state.
+	init () {}
 
-  	 preload () {
-		 game.stage.disableVisibilityChange = true;  // keeps everything running when the stage is no longer focused
-	 }
+	// Called after init(). Use for loading assets. Do not create objects in this function.
+	preload () {
+		//fixes the sound being suspended without a click in Chrome
+		if (game.sound.context.state === 'suspended') {
+			game.sound.context.resume();
+		}
+		game.stage.disableVisibilityChange = true;  // keeps everything running when the stage is no longer focused
+	}
 
-  	 create () {}
+	// Called immediately after preload() is completed. 
+	create () {}
+	
+	// Updates and redraws your game. Called every frame.
+	update () {}
 }
